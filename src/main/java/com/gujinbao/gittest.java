a597380fd0a3ef833a4f2bfa7df407e58e6b9e18
LinGenHart <LinGenHart812825035@163.com>
2023-03-23 22:00:29 +0800

    tharead commit

--- src/main/java/com/gujinbao/gittest.java
+++ src/main/java/com/gujinbao/gittest.java
@@ -3,5 +3,6 @@ package com.gujinbao;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("holle git");
+        System.out.println("holle git");
     }
 }
